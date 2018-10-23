f4c7deb28670d0df62b849366262bc1774370901
jasonren5 <31326470+jasonren5@users.noreply.github.com>
2018-10-22 23:13:15 -0500

    TODO: figure out why the length sometimes isn't right

--- Ren_Soundex.py
+++ Ren_Soundex.py
@@ -2,7 +2,7 @@ class soundexer:
     #length is the length of the soundex returned by the object
     def __init__(self, length=4):
         self.DICT = {}
-        #self.DICT.update(dict.fromkeys(["a", "e", "i", "o", "u", "y", "h", "w"], ""))
+        self.DICT.update(dict.fromkeys(["a", "e", "i", "o", "u", "y", "h", "w"], ""))
         self.DICT.update(dict.fromkeys(["b", "f", "p", "v"], "1"))
         self.DICT.update(dict.fromkeys(["c","g","j","k","q","s","x","z"], "2"))
         self.DICT.update(dict.fromkeys(["d", "t"], "3"))
@@ -15,6 +15,7 @@ class soundexer:
     #   and then joins that list at the end
     def soundex(self, string):
         counter = self.length
+        prevCoutner = counter
         returnSoundex = ""
         if string == "":
             return ""
@@ -22,24 +23,28 @@ class soundexer:
         for x in string:
             if x.isalpha():
                 if counter == self.length:
-                    tempSoundex.append(x)
+                    tempSoundex.append(x.upper())
                     counter -= 1
-                    continue
-                if x in self.DICT:
-                    if counter >= 0:
-                        tempSoundex.append(self.getNumber(x.lower()))
-                    counter -= 1
-            prev = x
+                    prevAppend = x.lower()
+                    prevCounter = counter
+                    #else if x in dict, multiple consecutive letters w/ same number, and letters w same number not separated with h or w
+                elif x in self.DICT and self.getNum(x.lower()) != self.getNum(prev) and ((self.getNum(x.lower()) != tempSoundex[len(tempSoundex)-1]) and (prev != "h" and prev !="w")):
+                    if counter > 0:
+                        #if not ((self.getNum(x.lower()) == tempSoundex[len(tempSoundex)-1]) or self.getNum(x.lower()) == self.getNum(prev)) and prevCounter == counter:
+                        tempSoundex.append(self.getNum(x.lower()))
+                        counter -= 1
+                        prevCounter = counter
+                prev = x.lower()
             if counter == 0:
                 break
         if len(tempSoundex) > 0:
-            while counter > 0:
+            while counter >= 0:
                 tempSoundex.append("0")
                 counter -= 1
         returnSoundex = "".join(tempSoundex)
         return returnSoundex
 
     #used in creating the soundex; returns the number from the dictionary
-    def getNumber(self, string):
+    def getNum(self, string):
         return self.DICT[string]
         
